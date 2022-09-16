24fb746f7c78f1b36987458f6ef4de854a56ebd7
jujunsi <113674833+jujunsi@users.noreply.github.com>
2022-09-16 12:05:55 +0200

    binaire vers décimal
    g rajouter 2 lignes à la fin

--- projet python NSI.py
+++ projet python NSI.py
@@ -1,14 +1,19 @@
-nombre = int (input ("entrez un entier Ã©crit en base 2, 10 ou 16 : "))
+nombre = int (input ("entrez un entier écrit en base 2, 10 ou 16 : "))
 
 base_de_depart = (input ("quelle est la base de ce nombre ? "))
 
 base_darrivee = (input ("dans quelle base voulez-vous le passer ? "))
- print ("bonjour")
 
-nombre final (nombre, base_de_depart, base_darrivee) :
-    if base_de_depart == 2
-    
-    
+
+nombre_final (nombre, base_de_depart, base_darrivee)
+if base_de_depart == 2 :
+    if base_darrivee == 2 :
+            nombre_final == nombre
+
+    if base_darrivee == 10 :
+            nombre_final == int ('nombre_de_depart',2)
+
+        
     
     
 
