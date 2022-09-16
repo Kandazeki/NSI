89f0c4a93706fef909ef5cde42003f41c124ea70
jujunsi <113674833+jujunsi@users.noreply.github.com>
2022-09-16 11:50:19 +0200

    TRUC QUE G FAIT AU D2BUT
    bfdjmvkfqjsw

---
+++ projet python NSI.py
@@ -0,0 +1,12 @@
+nombre = int (input ("entrez un entier écrit en base 2, 10 ou 16 : "))
+
+base_de_depart = (input ("quelle est la base de ce nombre ? "))
+
+base_darrivee = (input ("dans quelle base voulez-vous le passer ? "))
+
+
+nombre final (nombre, base_de_depart, base_darrivee) :
+    if base_de_depart == 2
+    
+    
+
