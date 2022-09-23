33a2e9ddfc45e6495b2ea1f083c0c757e4a024d3
jujunsi <113674833+jujunsi@users.noreply.github.com>
2022-09-23 11:59:45 +0200

    g mis le calcul binaire à la fin
    c pour passer de décimal à binaire pour les nombres jusqu'à 511c'est le seul morceau de code qui marche :)

--- projet python NSI.py
+++ projet python NSI.py
@@ -50,3 +50,79 @@ else :
 
 print (nombre_final)
 
+
+
+
+
+nombre = 511
+
+calc = nombre//2
+final = nombre%2
+
+if calc == 0 :
+    print (final)
+
+else :
+    calc2 = calc//2
+    final2 = calc%2
+
+    if calc2 == 0 :
+        print (final2, final)
+
+    else :
+        calc3 = calc2//2
+        final3 = calc2%2
+
+        if calc3 == 0 :
+            print (final3, final2, final)
+
+        else :
+            calc4 = calc3//2
+            final4 = calc3%2
+
+            if calc4 == 0 :
+                print (final4, final3, final2, final)
+
+            else :
+                calc5 = calc4//2
+                final5 = calc4%2
+
+                if calc5 == 0 :
+                    print (final5, final4, final3, final2, final)
+
+                else :
+                    calc6 = calc5//2
+                    final6 = calc5%2
+
+                    if calc6 == 0 :
+                        print (final6, final5, fianl4, final3, final2, final)
+
+                    else :
+                            calc7 = calc6//2
+                            final7 = calc6%2
+
+                            if calc7 == 0 :
+                                print (final7, final6, final5, final4, final3, final2, final)
+
+                            else :
+                                calc8 = calc7//2
+                                final8 = calc7%2
+
+                                if calc8 == 0 :
+                                    print (final8, final7, final6, final5, final4, final3, final2, final)
+
+                                else :
+                                    calc9 = calc8//2
+                                    final9 = calc8%2
+
+                                    if calc9 == 0 :
+                                        print (final9, final8, final7, final6, final5, final4, final3, final2, final)
+
+                                    else :
+                                        print ("Veuillez entrer un nombre plus petit que 511 s'il vous plait")
+        
+        
+
+
+
+
