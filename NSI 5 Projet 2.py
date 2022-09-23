11a04c2546cdde502bb67b828a731d88c996829f
Kandazeki <113674747+Kandazeki@users.noreply.github.com>
2022-09-23 11:51:14 +0200

    Add files via upload
    Voila c'est beau les ordinateurs qui sont des filles de joie

---
+++ NSI 5 Projet 2.py
@@ -0,0 +1,25 @@
+def p (entree):
+    print (str (entree))
+
+dict = {"a":11, "b":12, "c":13, "d":14, "e":15, "f":16}
+
+nombreDeDepart= (input("Entrez un nombre en base 2, 10 ou 16: "))
+premiereBase= (input("Entrez la base du nombre : 2, 10 ou 16: "))
+
+if nombreDeDepart==2, premiereBase==10
+
+
+
+
+
+
+
+
+
+
+
+                   
+
+
+
+
