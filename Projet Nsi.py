40df1dc4513a3768b104b068eaf8653ea701030a
Kandazeki <113674747+Kandazeki@users.noreply.github.com>
2022-09-30 23:05:52 +0200

    Add files via upload

---
+++ Projet Nsi.py
@@ -0,0 +1,12 @@
+x_seize = list(input("Entrez un nombre en base hexad�cimale s�par� aves des virgules: "))
+
+"a" = 10
+"b" = 11
+"c" = 12
+"d" = 13
+"e" = 14
+"f" = 15
+
+result = x_seize [3]*16**0 + x_seize [2]*16**1 + x_seize [1]*16**2 + x_seize [0]*16**3
+
+print (result)
